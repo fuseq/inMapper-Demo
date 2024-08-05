10eef95168de91b1a11df7d9c3afceae8471e463
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-08-05 08:49:09 +0300

    decreased

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function showArrow(direction) {
 
     directionIndicator.innerText = `Direction: ${direction.toFixed(2)}`;
 
-    if (direction < 50 || direction > 310) {
+    if (direction < 30 || direction > 320) {
         leftArrow.style.display = 'none';
         rightArrow.style.display = 'none';
         progressFrame.style.display = 'block';
