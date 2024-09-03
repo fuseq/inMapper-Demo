7299d9d2cb3d9d0baf865012e5332c00652e4e9c
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-03 09:00:14 +0300

    border test 12

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function setModel(model, entity) {
     let border = document.createElement('a-torus');
     border.setAttribute('position', '0 2 0');
     border.setAttribute('radius', '3'); // Adjust the radius
-    border.setAttribute('tube', '0.01'); // Smaller tube radius for thinner border
+    border.setAttribute('tube', '0.03'); // Smaller tube radius for thinner border
     border.setAttribute('rotation', '0 90 0'); // Adjust rotation as needed
     border.setAttribute('material', 'color: red; opacity: 0.5; wireframe: true'); // Styling
     
