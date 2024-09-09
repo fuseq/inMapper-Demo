a59e2ae785f62f4010c48407a5b105b2ef1e7768
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-09 11:51:22 +0300

    .

--- script.js
+++ script.js
@@ -240,8 +240,7 @@ navigator.geolocation.watchPosition(position => {
         const directionElement = document.getElementById('direction');
         const direction = getCompassDirection(compass);
         directionElement.textContent = direction;
-        const directionToTurn = (bearingToTarget - compass + 360) % 360; // 180 derece ekleyin
-        directionElement.textContent = directionToTurn;
+        const directionToTurn = (bearingToTarget + 360) % 360; // 180 derece ekleyin
         showArrow(directionToTurn,compass);
     });
 
