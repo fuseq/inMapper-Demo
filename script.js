5e33ba4b128542afe82af43294a65de5cb775551
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-16 11:12:05 +0300

    .

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let direction
 window.onload = () => {
     // Sayfa yüklendiğinde yerleri yükler ve mesafe kontrolünü başlatır
     let places = staticLoadPlaces(window.coords);
-    renderPlaces(places);
     startDistanceCheck(window.coords);
 };
 
@@ -75,20 +74,7 @@ function setModel(model, entity, rotation) {
 }
 
 // Yerleri sahnede render eder (görüntüler)
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
-        let rotation = calculateRotation();
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('look-controls','smoothing: 1')
-        setModel(models[modelIndex], model, rotation);
-        model.removeAttribute('animation-mixer');
-        scene.appendChild(model);
-    });
-}
+
 // İki koordinat arasındaki yönü hesaplar
 
 // Yön açısına göre pusula yönünü döndürür (örn: N, NE, E vb.)
