714c1943779fd7ca39104325aa601d65a2bbe9d0
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-16 09:49:53 +0300

    .

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/finish_pin.gltf',
-        scale: '5 5 5',
+        scale: '5 10 5',
         info: '',
         rotation: '0 0 0',
         position: '0 0 0',
