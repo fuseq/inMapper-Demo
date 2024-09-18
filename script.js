73384769845ab891c609e5ce3cc59ff7dfe75498
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-18 10:03:09 +0300

    .

--- script.js
+++ script.js
@@ -239,18 +239,8 @@ navigator.geolocation.watchPosition(position => {
     const { latitude, longitude } = position.coords;
     const targetLat = parseFloat(window.coords.x2);
     const targetLon = parseFloat(window.coords.y2);
-    const sourceLat = parseFloat(window.coords.x1);
-    const sourceLon = parseFloat(window.coords.y1);
     const bearingToTarget = calculateBearing(latitude, longitude, targetLat, targetLon);
-    const bearingToSource = calculateBearing(latitude, longitude, sourceLat, sourceLon);
-    const positionIndicator = document.getElementById('position-indicator');
-    const distanceIndicator = document.getElementById('distance-indicator');
-    const directionFromStartIndicator = document.getElementById('direction-from-start-indicator');
-    positionIndicator.innerText = `Position: ${latitude.toFixed(5)}, ${longitude.toFixed(5)}`;
-    const distance = calculateDistance(latitude, longitude, sourceLat, sourceLon);
-    distanceIndicator.innerText = `Distance: ${distance.toFixed(2)} meters`;
-    const directionFromStart = getDirectionFromBearing(bearingToSource);
-    directionFromStartIndicator.innerText = `Direction from Start: ${directionFromStart}`;
+   
 
     startCompassListener(compass => {
         const directionElement = document.getElementById('direction');
