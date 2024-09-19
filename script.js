f0726333c44810bcaa6ec140a6436244cd643383
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-19 15:27:01 +0300

    .

--- script.js
+++ script.js
@@ -261,7 +261,6 @@ function disableCamera() {
       }
     }
   }
-/*
 function handleOrientation(event) {
     const beta = event.beta; // Y eksenine göre eğim açısı (0 ile 180 derece arasında)
     const bottomContainer = document.querySelector('.bottom-container');
@@ -282,7 +281,6 @@ function handleOrientation(event) {
         isBetaAbove45 = false; // Beta 45'ten küçükse false yap
     }
 }
-    */
 navigator.geolocation.watchPosition(position => {
     const { latitude, longitude } = position.coords;
     const targetLat = parseFloat(window.coords.x2);
@@ -300,7 +298,7 @@ navigator.geolocation.watchPosition(position => {
     // const directionFromStart = getDirectionFromBearing(bearingToSource);
     // directionFromStartIndicator.innerText = `Direction from Start: ${directionFromStart}`;
 
-   /* window.addEventListener('deviceorientation', handleOrientation); */
+    window.addEventListener('deviceorientation', handleOrientation);
 
     startCompassListener(compass => {
        /* const directionElement = document.getElementById('direction');
