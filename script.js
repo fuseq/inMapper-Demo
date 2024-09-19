51782378bd7886459d3ac97c62995ce1a394886e
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-19 15:30:32 +0300

    .

--- script.js
+++ script.js
@@ -269,16 +269,19 @@ function handleOrientation(event) {
 
     // Eğer beta değeri 45 dereceden büyükse
     if (beta > 45) {
-        bottomContainer.style.height = '30%';
+       /* bottomContainer.style.height = '30%';
         mapSection.style.height = '100%';
         
         isBetaAbove45 = true;  // Beta 45'ten büyükse true yap  
         disableCamera();
+        */
     } else {
+        /*
         bottomContainer.style.height = '100%';
         mapSection.style.height = '100%';
         
         isBetaAbove45 = false; // Beta 45'ten küçükse false yap
+        */
     }
 }
 navigator.geolocation.watchPosition(position => {
