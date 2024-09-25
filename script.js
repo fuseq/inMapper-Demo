ff4d634e01c8a648689a1efd8e4cc67295f4a816
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-25 10:32:31 +0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let stepCount = 0;
 let lastAlpha = null;
-let movementThreshold = 5;
+let movementThreshold = 2.5;
 let directionMatches = false;
 let stepIncreaseAllowed = true;
 let isLoading = false;
